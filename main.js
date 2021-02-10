61c23653384110c01b8e4e1dbc2e16600f77da9a
Heidi Hood <heidih74@gmail.com>
2021-02-10 17:48:42 -0500

    final-code

--- main.js
+++ main.js
@@ -1,66 +1,88 @@
 // Yell real loud
-let yellButton = document.getElementById('yell')
-yellButton.addEventListener('click', () => {
-  yell()
-})
+let yellButton = document.getElementById("yell");
+yellButton.addEventListener("click", () => {
+  yell();
+});
 
 let yell = () => {
-  // your code here
-}
+  document.querySelector(".page-header").textContent = "MY NAME IS GEORGE!!!!!";
+};
 
 // Make me fierce
-let fierceButton = document.getElementById('fierceify')
-fierceButton.addEventListener('click', () => {
-  fierceify()
-})
+let fierceButton = document.getElementById("fierceify");
+fierceButton.addEventListener("click", () => {
+  fierceify();
+});
 
 let fierceify = () => {
-  // your code here
-}
+  document
+    .getElementsByTagName("img")[0]
+    .setAttribute(
+      "src",
+      "https://pbs.twimg.com/profile_images/1340495732885737479/HB9D5kGs_400x400.jpg"
+    );
+  // document.querySelector(".headshot").src = "https://pbs.twimg.com/profile_images/1340495732885737479/HB9D5kGs_400x400.jpg"
+};
 
 // Reveal an Easter egg
-let easterEggButton = document.getElementById('easter-egg')
-easterEggButton.addEventListener('click', () => {
-  eggify()
-})
+let easterEggButton = document.getElementById("easter-egg");
+easterEggButton.addEventListener("click", () => {
+  eggify();
+});
 
 let eggify = () => {
-  // your code here
-}
+  let easterEggImage = document.createElement("img");
+  easterEggImage.setAttribute(
+    "src",
+    "https://d1e3z2jco40k3v.cloudfront.net/-/media/mccormick-us/recipes/mccormick/t/800/two-toned-easter-eggs.jpg"
+  );
+  easterEggImage.style.width = "200px";
+  // easterEggImage.style.border = "2px solid red";
+  let parent = document.getElementsByTagName("body")[0];
+  // let parent = document.querySelector("body")
+  parent.appendChild(easterEggImage);
+};
 
 // Select the first paragraph in the document
-let selectMeButton = document.getElementById('select-me')
-selectMeButton.addEventListener('click', () => {
-  selectFirstParagraph()
-})
+let selectMeButton = document.getElementById("select-me");
+selectMeButton.addEventListener("click", () => {
+  selectFirstParagraph();
+});
 
 let selectFirstParagraph = () => {
-  // your code here
-}
+  document.querySelector("p").classList += "selected";
+};
 
 // Deselect the first paragraph in the document
-let deselectMeButton = document.getElementById('deselect-me')
-deselectMeButton.addEventListener('click', () => {
-  deselectFirstParagraph()
-})
+let deselectMeButton = document.getElementById("deselect-me");
+deselectMeButton.addEventListener("click", () => {
+  deselectFirstParagraph();
+});
 
 let deselectFirstParagraph = () => {
-  // your code here
-}
+  document.querySelector("p").classList.remove("selected");
+  // document.querySelector("p").className = ""
+};
 
 // Toggle the visibility of the image of your face
-let toggleFaceButton = document.getElementById('toggle-my-face')
-toggleFaceButton.addEventListener('click', () => {
-  toggleMyFace()
-})
+let toggleFaceButton = document.getElementById("toggle-my-face");
+toggleFaceButton.addEventListener("click", () => {
+  toggleMyFace();
+});
 
 let toggleMyFace = () => {
-  // your code here
-}
+  let headshot = document.querySelector(".headshot");
+  // headshot.classList.toggle("hidden")
+  if (headshot.classList.contains("hidden")) {
+    headshot.classList.remove("hidden");
+  } else {
+    headshot.classList.add("hidden");
+  }
+};
 
 // Reload page when reload button is clicked -- no need to modify anything here!
-let reloadButton = document.getElementById('reload')
+let reloadButton = document.getElementById("reload");
 
-reloadButton.addEventListener('click', () => {
-  location.reload()
-})
+reloadButton.addEventListener("click", () => {
+  location.reload();
+});
